3310d3cedab891411a06d305bc6d2d3bfa3af31a
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-09-02 20:01:15 -0300

    fix: ci pipe

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -16,37 +16,7 @@ resource "aws_s3_bucket" "coingecko-storage" {
 
 resource "aws_s3_bucket_policy" "coingecko-storage" {
   bucket = aws_s3_bucket.coingecko-storage.id
-  policy = jsonencode({
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowGlueJobsAndLambdaAccess",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": [
-          "arn:aws:iam::663354324751:role/glue-bronze-role",
-          "arn:aws:iam::663354324751:role/glue-silver-role",
-          "arn:aws:iam::663354324751:role/lambda-ingestion-role"
-        ]
-      },
-      "Action": [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject",
-        "s3:ListBucket"
-      ],
-      "Resource": [
-        "arn:aws:s3:::coingecko-storage-663354324751",
-        "arn:aws:s3:::coingecko-storage-663354324751/*"
-      ],
-      "Condition": {
-        "StringEquals": {
-          "aws:SourceAccount": "663354324751"
-        }
-      }
-    }
-  ]
-})
+  policy = file("${path.module}/policies/coingecko-storage-policy.json")
 }
 
 # Bucket de logs
@@ -63,23 +33,5 @@ resource "aws_s3_bucket_logging" "data_logging" {
 
 resource "aws_s3_bucket_policy" "log_bucket_policy" {
   bucket = aws_s3_bucket.log_bucket.id
-  policy = jsonencode({
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "S3ServerAccessLogsPolicy",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "logging.s3.amazonaws.com"
-      },
-      "Action": "s3:PutObject",
-      "Resource": "arn:aws:s3:::logs-coingecko-storage-663354324751/logs/*",
-      "Condition": {
-        "StringEquals": {
-          "aws:SourceAccount": "663354324751"
-        }
-      }
-    }
-  ]
-})
+  policy = file("${path.module}/policies/log-bucket-policy.json")
 }
