e75d66afa3dd8d640481ca6e4966ad32f3f876fd
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-09-01 23:55:49 -0300

    build: add modules terraform

--- infra/main.tf
+++ infra/main.tf
@@ -6,13 +6,13 @@ provider "aws" {
 
 terraform {
   required_version = ">= 1.5.0"
-
-#  backend "s3" {
-#    bucket         = "coingecko-states-663354324751"
-#    key            = "data-pipeline/terraform.tfstate"
-#    region         = "us-east-1"
-#    encrypt        = true
-#  }
+  
+  backend "s3" {
+    bucket         = "coingecko-states-663354324751"
+    key            = "data-pipeline/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 }
 
 # Tags globais
@@ -22,4 +22,8 @@ locals {
     Environment = terraform.workspace
     Owner       = "time-data"
   }
+}
+
+module "s3" {
+  source = "./s3"
 }
