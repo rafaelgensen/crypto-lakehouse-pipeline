35e49ab0108a6991096a589a3126c3c1a83db8a8
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-09-12 20:33:47 -0300

    fix: API key for data ingestion

---
+++ infra/modules/stepfunc/variables.tf
@@ -0,0 +1,13 @@
+variable "API_KEY_CG_STEP" {
+  description = "API key for external service"
+  type        = string
+  sensitive   = true
+}
+
+variable "teste" {
+  type = int
+  default = 5
+
+}
+
+var.teste
