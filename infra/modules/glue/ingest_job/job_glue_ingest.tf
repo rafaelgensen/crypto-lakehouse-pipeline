e9583c64994cb6683e9a05b26bc8ce41b4d7173e
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-09-28 15:40:39 -0300

    fix: API key glue ingest

--- infra/modules/glue/ingest_job/job_glue_ingest.tf
+++ infra/modules/glue/ingest_job/job_glue_ingest.tf
@@ -11,33 +11,31 @@ resource "aws_s3_bucket_policy" "glue-scripts" {
 
 # Upload do script
 resource "aws_s3_object" "glue_ingest_script" {
-  bucket = aws_s3_bucket.glue-scripts.id 
-  key    = "glue_job_ingest.py"
-  source = "${path.module}/glue_job_ingest.py"
+  bucket      = aws_s3_bucket.glue-scripts.id 
+  key         = "glue_job_ingest.py"
+  source      = "${path.module}/glue_job_ingest.py"
+  source_hash = filebase64sha256("${path.module}/glue_job_ingest.py")
 }
 
-# Glue Job
 resource "aws_glue_job" "ingest" {
+  depends_on = [aws_s3_object.glue_ingest_script]
   name     = "coingecko-ingest-etl"
   role_arn = aws_iam_role.glue_ingest_role.arn
 
   command {
     name            = "glueetl"
     script_location = "s3://${aws_s3_bucket.glue-scripts.bucket}/glue_job_ingest.py"
-    #script_location = "s3://coingecko-glue-scripts-663354324751/glue_job_ingest.py"
     python_version  = "3"
   }
 
-  glue_version = "5.0"
-  worker_type = "G.1X"
+  glue_version      = "5.0"
+  worker_type       = "G.1X"
   number_of_workers = 2
-  description  = "Ingest from Coingecko API"
-  
+  description       = "Ingest from Coingecko API"
 
-    default_arguments = {
-    "--enable-metrics"      = "true"                # send metrics to CloudWatch
-    "--TempDir"             = "s3://coingecko-staging-663354324751/temp/"
+  default_arguments = {
+    "--enable-metrics" = "true"
+    "--TempDir"        = "s3://coingecko-staging-663354324751/temp/"
     "--job-language"   = "python"
   }
-
 }
