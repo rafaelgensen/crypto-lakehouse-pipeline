583903d74abc4893fe4d26ed21b37d19380eab4f
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-09-28 22:28:38 -0300

    build: Lambda bootstrap function

--- infra/modules/redshift/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "namespace_name" {
-  value = aws_redshiftserverless_namespace.this.namespace_name
-}
-
-output "workgroup_name" {
-  value = aws_redshiftserverless_workgroup.this.workgroup_name
-}
