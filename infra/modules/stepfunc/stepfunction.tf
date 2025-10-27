2801cd478666effc9327b58d3bc89d39620ce622
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-10-27 00:02:11 -0300

    refactor(stepfunctions): lambda starts parallel with bronze glue job

--- infra/modules/stepfunc/stepfunction.tf
+++ infra/modules/stepfunc/stepfunction.tf
@@ -2,85 +2,106 @@ resource "aws_sfn_state_machine" "glue_etl_pipeline" {
   name     = "coingecko-etl-pipeline"
   role_arn = aws_iam_role.step_functions_role.arn
 
-  definition = jsonencode({
-    Comment = "ETL Pipeline: Glue Ingest -> Bronze -> Lambda -> Silver -> Gold",
-    StartAt = "GlueJobBIngest",
-    States = {
-      GlueJobBIngest = {
-        Type       = "Task",
-        Resource   = "arn:aws:states:::glue:startJobRun.sync",
-        Parameters = {
-          JobName = "coingecko-ingest-etl"
-        },
-        Next = "GlueJobBronze",
-        Catch = [{
-          ErrorEquals = ["States.ALL"],
-          ResultPath  = "$.error",
-          Next        = "FailState"
-        }]
+definition = jsonencode({
+  Comment = "ETL Pipeline: Glue Ingest -> (Bronze + Lambda) -> Silver -> Gold",
+  StartAt = "GlueJobBIngest",
+  States = {
+    GlueJobBIngest = {
+      Type       = "Task",
+      Resource   = "arn:aws:states:::glue:startJobRun.sync",
+      Parameters = {
+        JobName = "coingecko-ingest-etl"
       },
-      GlueJobBronze = {
-        Type       = "Task",
-        Resource   = "arn:aws:states:::glue:startJobRun.sync",
-        Parameters = {
-          JobName = "coingecko-bronze-etl"
-        },
-        Next = "LambdaBootstrap",
-        Catch = [{
-          ErrorEquals = ["States.ALL"],
-          ResultPath  = "$.error",
-          Next        = "FailState"
-        }]
-      },
-      LambdaBootstrap = {
-        Type       = "Task",
-        Resource   = "arn:aws:states:::lambda:invoke",
-        Parameters = {
-          FunctionName = var.lambda_function_arn,
-          Payload = {
-            action = "create_schemas"
+      Next = "ParallelBronzeAndLambda",
+      Catch = [{
+        ErrorEquals = ["States.ALL"],
+        ResultPath  = "$.error",
+        Next        = "FailState"
+      }]
+    },
+    
+    ParallelBronzeAndLambda = {
+      Type = "Parallel",
+      Branches = [
+        {
+          StartAt = "GlueJobBronze",
+          States = {
+            GlueJobBronze = {
+              Type       = "Task",
+              Resource   = "arn:aws:states:::glue:startJobRun.sync",
+              Parameters = {
+                JobName = "coingecko-bronze-etl"
+              },
+              End = true,
+              Catch = [{
+                ErrorEquals = ["States.ALL"],
+                ResultPath  = "$.error",
+                Next        = "FailState"
+              }]
+            }
           }
         },
-        Next = "GlueJobSilver",
-        Catch = [{
-          ErrorEquals = ["States.ALL"],
-          ResultPath  = "$.error",
-          Next        = "FailState"
-        }]
+        {
+          StartAt = "LambdaBootstrap",
+          States = {
+            LambdaBootstrap = {
+              Type       = "Task",
+              Resource   = "arn:aws:states:::lambda:invoke",
+              Parameters = {
+                FunctionName = var.lambda_function_arn,
+                Payload = {
+                  action = "create_schemas"
+                }
+              },
+              End = true,
+              Catch = [{
+                ErrorEquals = ["States.ALL"],
+                ResultPath  = "$.error",
+                Next        = "FailState"
+              }]
+            }
+          }
+        }
+      ],
+      Next = "GlueJobSilver"
+    },
+    # --- fim do paralelo ---
+
+    GlueJobSilver = {
+      Type       = "Task",
+      Resource   = "arn:aws:states:::glue:startJobRun.sync",
+      Parameters = {
+        JobName = "coingecko-silver-etl"
       },
-      GlueJobSilver = {
-        Type       = "Task",
-        Resource   = "arn:aws:states:::glue:startJobRun.sync",
-        Parameters = {
-          JobName = "coingecko-silver-etl"
-        },
-        Next = "GlueToGold",
-        Catch = [{
-          ErrorEquals = ["States.ALL"],
-          ResultPath  = "$.error",
-          Next        = "FailState"
-        }]
+      Next = "GlueToGold",
+      Catch = [{
+        ErrorEquals = ["States.ALL"],
+        ResultPath  = "$.error",
+        Next        = "FailState"
+      }]
+    },
+
+    GlueToGold = {
+      Type       = "Task",
+      Resource   = "arn:aws:states:::glue:startJobRun.sync",
+      Parameters = {
+        JobName = "coingecko-gold-etl"
       },
-      GlueToGold = {
-        Type       = "Task",
-        Resource   = "arn:aws:states:::glue:startJobRun.sync",
-        Parameters = {
-          JobName = "coingecko-gold-etl"
-        },
-        End = true,
-        Catch = [{
-          ErrorEquals = ["States.ALL"],
-          ResultPath  = "$.error",
-          Next        = "FailState"
-        }]
-      },
-      FailState = {
-        Type  = "Fail",
-        Cause = "ETL Job Failed",
-        Error = "JobExecutionError"
-      }
+      End = true,
+      Catch = [{
+        ErrorEquals = ["States.ALL"],
+        ResultPath  = "$.error",
+        Next        = "FailState"
+      }]
+    },
+
+    FailState = {
+      Type  = "Fail",
+      Cause = "ETL Job Failed",
+      Error = "JobExecutionError"
     }
-  })
+  }
+})
 
   type = "STANDARD"
 }
