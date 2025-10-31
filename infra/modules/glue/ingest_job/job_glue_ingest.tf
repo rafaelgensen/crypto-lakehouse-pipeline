6a57857031c0a9412a792a4850d95e57342694b4
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-10-31 01:45:23 -0300

    build: new ci workflow backend + bucket for glue scripts

--- infra/modules/glue/ingest_job/job_glue_ingest.tf
+++ infra/modules/glue/ingest_job/job_glue_ingest.tf
@@ -1,17 +1,6 @@
-# Bucket para armazenar scripts do Glue
-resource "aws_s3_bucket" "glue-scripts" {
-  bucket = "coingecko-glue-scripts-663354324751"
-}
-
-# Access policy
-resource "aws_s3_bucket_policy" "glue-scripts" {
-  bucket = aws_s3_bucket.glue-scripts.id
-  policy = file("${path.module}/policy_scripts_bucket.json")
-}
-
 # Upload do script
 resource "aws_s3_object" "glue_ingest_script" {
-  bucket      = aws_s3_bucket.glue-scripts.id 
+  bucket      = "coingecko-glue-scripts-663354324751"
   key         = "glue_job_ingest.py"
   source      = "${path.module}/glue_job_ingest.py"
   source_hash = filebase64sha256("${path.module}/glue_job_ingest.py")
