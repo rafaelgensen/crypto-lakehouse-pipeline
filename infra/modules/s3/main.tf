1141d4780ec6ed35965eb8d2e7beb3b7ba8314b2
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-10-31 01:45:40 -0300

    build: new ci workflow backend + bucket for glue scripts

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -1,3 +1,15 @@
+# Bucket para armazenar scripts do Glue
+resource "aws_s3_bucket" "glue-scripts" {
+  bucket = "coingecko-glue-scripts-663354324751"
+}
+
+# Access policy
+resource "aws_s3_bucket_policy" "glue-scripts" {
+  bucket = aws_s3_bucket.glue-scripts.id
+  policy = file("${path.module}/policies/policy_scripts_bucket.json")
+}
+
+
 # Bucket Coin-Gecko - Staging
 
 resource "aws_s3_bucket" "coingecko-staging" {
